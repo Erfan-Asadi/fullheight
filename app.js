e12d1999453b55c2539bd31243bc3d53df21c02a
Erfan Asadi <asadi.love7981@gmail.com>
2022-06-19 20:38:20 +0430

    comment scripts

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 "use strict";
 
-    document.documentElement.style.setProperty('--visual-height', `${visualViewport.height}px`);
+    // document.documentElement.style.setProperty('--visual-height', `${visualViewport.height}px`);
